b206c04c8dbda470b6088a5959c103775edafe38
ErwanQui <erwan.quinot@gmail.com>
2023-08-17 16:03:13 +0200

    again

--- server/src/food.js
+++ server/src/food.js
@@ -17,7 +17,7 @@ router.get('/id', async (req, res) => {
   const { id } = req.query;
   try {
     await checkConnection(req);
-    const food = await Food.findOne({_id: id}).populate('craft.element');
+    const food = await Food.findOne({_id: id.toString()}).populate('craft.element');
     res.json(food);
   } catch (error) {
     res.status(403).send(error);
