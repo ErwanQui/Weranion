3aae829afe4dd4bd8ab1d2751d186b0d4a1c14ae
ErwanQui <erwan.quinot@gmail.com>
2023-09-07 14:24:41 +0200

    add real time chat template

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,10 +1,10 @@
 import { useNavigate } from 'react-router-dom'; // Add this
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from '../../api';
 import checkConnection from '../../utils/authentification';
 import { Fab } from '@mui/material';
 import { AttachMoney, HomeOutlined, LocationCity, Map, MenuBookOutlined, Person } from '@mui/icons-material';
-
+import Ably from 'ably';
 import './Main.scss';
 import { PlayerData } from '../../models/playerData.model';
 // import { Button, TextField } from '@mui/material';
@@ -48,11 +48,64 @@ function Main() {
       { player ? (player.mj ? 
         <div className='mjMenus'>MJ</div> : null) : null}
       <div className='playingSide'>
-
+        <ChatApp></ChatApp>
       </div>
       <div className='playerSide'>
-
+        <Fab></Fab>
       </div>
+    </div>
+  );
+}
+
+
+function ChatApp() {
+  const [messages, setMessages] = useState<any[]>([]);
+  const [messageInput, setMessageInput] = useState('');
+  const [ablyClient, setAblyClient] = useState<Ably.Realtime | null>(null);
+
+  useEffect(() => {
+    // Initialisation d'Ably avec vos clés d'API (côté client)
+    const apiKey = process.env.REACT_APP_ABLY_API_KEY;
+    const client = new Ably.Realtime({ key: apiKey });
+    setAblyClient(client);
+
+    // Récupérer le canal de chat
+    const channel = client.channels.get('chat');
+
+    // Écouter les messages entrants
+    channel.subscribe('message', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message.data]);
+    });
+
+    return () => {
+      // Fermer la connexion Ably lorsqu'on quitte le composant
+      client.close();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (ablyClient) {
+      const channel = ablyClient.channels.get('chat');
+      channel.publish('message', messageInput);
+      setMessageInput('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Chat en temps réel avec React</h1>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
+      <input
+        type="text"
+        placeholder="Entrez votre message"
+        value={messageInput}
+        onChange={(e) => setMessageInput(e.target.value)}
+      />
+      <button onClick={sendMessage}>Envoyer</button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 // console.log(process.env.HARPERDB_URL); // remove this after you've confirmed it working
 const express = require('express');
 const app = express();
+const Ably = require('ably');
 // const http = require('http');
 const cors = require('cors');
 const cp = require('cookie-parser');
@@ -17,6 +18,10 @@ const TreasurySheet = require('./models/treasurySheet');
 
 
 // const Person = require('./models/person');
+
+const ably = new Ably.Rest({
+  key: process.env.ABLY_API_KEY,
+});
 
 app.use(
   cors({
@@ -52,6 +57,15 @@ app.get('/', async (req, res) => {
   //   .populate('investments');
   // res.json(test);
   res.json('hello');
+});
+
+// Endpoint pour gérer la communication en temps réel
+app.get('/publish', (req, res) => {
+  const channel = ably.channels.get('chat');
+
+  // Envoyer un message à tous les clients connectés au canal
+  channel.publish('message', 'Hello, Ably World!');
+  res.send('Message envoyé');
 });
 
 // app.get('/patate', (req, res) => {
