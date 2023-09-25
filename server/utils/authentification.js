dcd12e2b32eb7d5141eb08108d257e2d90755c15
ErwanQui <erwan.quinot@gmail.com>
2023-09-25 10:59:59 +0200

    Update authentification.js

--- server/utils/authentification.js
+++ server/utils/authentification.js
@@ -4,18 +4,18 @@ const jwt = require('jsonwebtoken');
 async function checkConnection(req) {
   return new Promise((resolve, reject) => {
     const token = req.cookies.token;
-    resolve(true);
-    // if (token) {
-    //   jwt.verify(token, process.env.JWT_SECRET, (err) => {
-    //     if (err) {
-    //       reject('Not connected');
-    //     } else {
-    //       resolve(true);
-    //     }
-    //   });
-    // } else {
-    //   reject('Not connected');
-    // }
+    // resolve(true);
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET, (err) => {
+        if (err) {
+          reject('Not connected');
+        } else {
+          resolve(true);
+        }
+      });
+    } else {
+      reject('Not connected');
+    }
   });
 }
 
